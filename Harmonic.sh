798e58879f767a6d4210bf2e11bd2a39c2c62237
Dewanshu Rahul <dewanshurahul95@gmail.com>
2020-02-07 12:36:23 +0530

    [Dewanshu Rahul]Refactor. Implemented bc command Correctly of Harmonic Program

--- Harmonic.sh
+++ Harmonic.sh
@@ -1,18 +1,20 @@
-#! /usr/bin/bash
+#! /bin/bash
 count=$1
 function error () {
 	echo "Input can not be 0 or Negative"
 }
 function harmonic () {
 	local num=$1 
-	local var=0
+	var=0
 	local count=1
 	local c=1
 	while (( $count <= $num ))
 	do
-		var=$(( $var + (( $c / $count )) ))
-		echo $var
+		y=`echo $c / $count | bc -l`
+		var=`echo $var + $y | bc -l`
+		(( count++ ))
 	done
+	echo "$var"
 }
 if [ $count -gt 0 ] 
 then
